ebf32de1adb09793dd2929d285a9d355745f428c
alvaro <alvaro@example.com>
2026-01-29 10:18:49 +0100

    Clase de programacion 29/01

--- src/GestionScanner/GestionScanner.java
+++ src/GestionScanner/GestionScanner.java
@@ -2,12 +2,22 @@
 
 package GestionScanner;
 
-/**
- *Nigga
- * @author Alvaro
- * @url youtube.com
- * @date29 ene 2026
- */
-public class GestionScanner {
+import java.util.Scanner;
 
+public class GestionScanner {
+    public static void main(String[] args) {
+        Scanner sc;
+        try {
+            sc = new Scanner(System.in);
+            System.out.println("Introduce un número: ");
+            int numero = sc.nextInt();
+            System.out.println("Has introducido:"+numero);
+        } catch (Exception e) {
+            System.out.println("Error: Debes introducir un número");
+        } finally {
+            System.out.println("Este mensaje aparece siempre");
+            //Aquí se liberarían recursos 
+        }
+        System.out.println("El programa continua");
+    }
 }
