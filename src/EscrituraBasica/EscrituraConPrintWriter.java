f4d6f19dc85829dc6223931f1a32d8269a0d7354
alvaro <alvaro@example.com>
2026-02-09 11:52:25 +0100

    Descarga

--- src/EscrituraBasica/EscrituraConPrintWriter.java
+++ src/EscrituraBasica/EscrituraConPrintWriter.java
@@ -2,12 +2,16 @@
 
 package EscrituraBasica;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class EscrituraConPrintWriter {
-    public static void main(String[] args) {
-        try {
-            
+    public static void main(String[] args) throws IOException {
+        try (PrintWriter pw = new PrintWriter(new FileWriter("informe.txt")){
+          
         } catch (Exception e) {
         }
-    }
-
+      }
+   
 }
--- src/EscrituraBasica/EscrituraFichero.java
+++ src/EscrituraBasica/EscrituraFichero.java
@@ -2,12 +2,21 @@
 
 package EscrituraBasica;
 
-/**
- *Nigga
- * @author Alvaro
- * @url youtube.com
- * @date4 feb 2026
- */
-public class EscrituraFichero {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class EscrituraFichero {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("alumnos.txt"))){
+            String linea;
+            int contador=1;
+            
+            //lee línea a línea hasta el final
+            while ((linea=br.readLine())!=null){
+                System.out.println(contador+"."+linea);
+                contador++;
+            }
+        } catch (Exception e) {
+        }
+    }
 }
